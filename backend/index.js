1e09907ff72f7048c1c9e89ad77b290429b81d2f
tandin2000 <taandinwangchen27@icloud.com>
2022-10-30 15:30:40 +0530

    cros added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var cors = require('cors');
 const userRoutes = require("./userRoutes");
 
 const app = express();
@@ -8,6 +9,8 @@ mongoose
   .connect("mongodb+srv://tandin:Reskar@27@db.soqaf.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
   .then((_) => console.log("Connected to DB"))
   .catch((err) => console.error("error", err));
+
+app.use(cors())
 
 app.use(express.json());
 app.use("/auth", userRoutes);
