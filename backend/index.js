200ab83f0d8d1d8b5bb09284126f6b9ae2f627dc
AnawaratneMANA <it19086408@my.sliit.lk>
2022-11-06 00:25:18 -0400

    Add dotenv to the backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./userRoutes");
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
