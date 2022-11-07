110c147e4b80a8f833557b0e7e778abed0861a43
AnawaratneMANA <it19086408@my.sliit.lk>
2022-11-07 21:14:54 +0530

    Add Message Schema backend

---
+++ backend/messageModel.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema({
+    userId: String,
+    message: String
+});
+
+module.exports = mongoose.model("message", MessageSchema);
